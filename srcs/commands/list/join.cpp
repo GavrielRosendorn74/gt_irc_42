b89dd3b41ccea4dfab704e3f999f3b01071e5f2a
GavrielRosendorn74 <rosendorngavrielofficial@gmail.com>
2022-08-09 16:57:22 +0200

    PP

--- srcs/commands/list/join.cpp
+++ srcs/commands/list/join.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   join.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: grosendo <grosendo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 08:20:15 by grosendo          #+#    #+#             */
-/*   Updated: 2022/08/09 14:53:43 by tanguy           ###   ########.fr       */
+/*   Updated: 2022/08/09 16:56:11 by grosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,36 @@ void Command::_join()
 		return;
 	}
 
-	std::string name = _args[0];
-	std::string password = _args.size() > 1 ? _args[1] : "";
+	string all_args = _args[0];
+	string password = _args.size() > 1 ? _args[1] : "";
 
+	while (all_args.find(',') != string::npos)
+	{
+		string name = all_args.substr(0, all_args.find(','));
+		
+		Channel *channel = _server->findChannelByName(name);
+
+		if (!channel)
+			channel = _server->createChannel(name, _client);
+
+		if (channel->getMaxClients() > 0 && channel->getNumClients() >= channel->getMaxClients()){
+			_client->reply(ERR_CHANNELISFULL(_client->getNickname(), name));
+			return;
+		}
+
+		if (channel->isCLientInChannel(_client))
+		{
+			_client->reply(ERR_USERONCHANNEL(_client->getNickname(), channel->getName()));
+			return ;
+		}
+
+		_client->join(channel);	
+		_client->write(RPL_NAMREPLY(_client->getNickname(), channel->getName(), channel->getClientsStr()));
+		_client->write(RPL_ENDOFNAMES(_client->getNickname(), channel->getName()));
+		all_args = all_args.substr(all_args.find(',') + 1);
+	}
+	string name = all_args.substr(0, all_args.find(','));
+		
 	Channel *channel = _server->findChannelByName(name);
 
 	if (!channel)
